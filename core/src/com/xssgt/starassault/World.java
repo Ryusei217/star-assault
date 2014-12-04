391adfff66e5a7df848e52b9ca462d4269cbf657
Cristian Calderón <exodus_c217@hotmail.com>
2014-12-04 14:24:47 -0600

    Corregido bug de los bloques faltantes
    - Ahora se deberia dibujar el escenario completamente.
    - Corregida la resolucion de la aplicacion de escritorio.

--- core/src/com/xssgt/starassault/World.java
+++ core/src/com/xssgt/starassault/World.java
@@ -30,12 +30,20 @@ public class World {
 	private void createDemoWorld() {
 		bob = new Bob(new Vector2(7,2));
 		
-		for (int i = 0; i < 10; i++) {
-			blocks.add(new Block(new Vector2(i, 0)));                       
-			blocks.add(new Block(new Vector2(i, 7)));                       
+		for (int i = 0; i < 10; i++) { 			 			
+			blocks.add(new Block(new Vector2(i, 0))); 			 			
+			blocks.add(new Block(new Vector2(i, 6))); 			 			
 			if (i > 2)
 				blocks.add(new Block(new Vector2(i, 1)));
-				
 		}
+		
+		blocks.add(new Block(new Vector2(9, 2)));
+		blocks.add(new Block(new Vector2(9, 3)));
+		blocks.add(new Block(new Vector2(9, 4)));
+		blocks.add(new Block(new Vector2(9, 5)));
+
+		blocks.add(new Block(new Vector2(6, 3)));
+		blocks.add(new Block(new Vector2(6, 4)));
+		blocks.add(new Block(new Vector2(6, 5)));
 	}
 }
--- desktop/src/com/xssgt/starassault/desktop/DesktopLauncher.java
+++ desktop/src/com/xssgt/starassault/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 	      config.title = "StarAssault";
-	      config.width = 800;
-	      config.height = 480;
+	      config.width = 1024;
+	      config.height = 576;
 	      new LwjglApplication(new StarAssault(), config);
 	}
 }
